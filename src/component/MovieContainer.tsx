ce334e0102d5c18c61760b5563b8a7c03bd6fde7
luthfikamalananda <luthfi_kamal_ananda@yahoo.com>
2024-04-18 17:59:25 +0700

    confused parah euy, kok ndak mau nge scale jadi besar ketutupan overflow

--- src/component/MovieContainer.tsx
+++ src/component/MovieContainer.tsx
@@ -2,11 +2,22 @@ import { Key } from "react";
 import { Link } from "react-router-dom";
 
 export default function MovieContainer({movieList}: {movieList:any}) {
+  
   return (
+    // const buttonRight = document.getElementById('slideRight');
+    // const buttonLeft = document.getElementById('slideLeft');
+    
+
+    // buttonRight.onclick = function () {
+    //   document.getElementById('container').scrollLeft += 20;
+    // };
+    // buttonLeft.onclick = function () {
+    //   document.getElementById('container').scrollLeft -= 20;
+    // };
     <>
       {movieList.map((movie: { id: Key | null | undefined; poster_path: any; }) => (
-        <div key={movie.id} className={`movie-card h-full flex-none hover:scale-100`}>
-          <Link to={`/homepage/${movie.id}`}><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="h-60 hover:scale-150 duration-150" alt="img" /></Link>
+        <div key={movie.id} className={`movie-card h-full flex-none`}>
+          <Link to={`/homepage/${movie.id}`}><img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="h-60 hover:scale-125 duration-150 " alt="img" /></Link>
         </div>
         
       ))}
